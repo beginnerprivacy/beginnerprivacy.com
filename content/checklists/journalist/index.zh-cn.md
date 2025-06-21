1a45162001572971ee98e7df1a4bb66fcdea9ae7
PrintN <127101769+PrintN@users.noreply.github.com>
2025-06-21 20:40:06 +0200

    checklist: Add hacker + journalist

--- content/checklists/journalist/index.zh-cn.md
+++ content/checklists/journalist/index.zh-cn.md
@@ -1,4 +1,6 @@
 ---
 title: 记者
 ---
-即将推出！
+{{< hextra/checklist-item link="../articles/ditch-sms-and-use-secure-communication-methods" text="使用端到端加密的消息应用程序与您的消息来源进行沟通。" id="only-use-encrypted-communication" >}}仅使用加密通信{{< /hextra/checklist-item >}}
+
+{{< hextra/checklist-item link="../articles/how-and-why-backing-up-your-devices-is-crucial" text="备份您的工作，以防被盗或丢失。" id="back-up-your-phone" >}}备份您的工作{{< /hextra/checklist-item >}}
--- public/js/main.js
+++ public/js/main.js
@@ -479,11 +479,27 @@ sections.forEach(section => {
     observerScroll.observe(section);
 });
 
+let isSearchInputFocused = false;
+
 if (window.location.pathname === '/' || window.location.pathname === '/es/' || window.location.pathname === '/zh-cn/') {
     document.querySelector('html').style.overflow = "hidden";
+
+    const searchInput = document.querySelector('.search-input')
+    const searchResults = document.querySelector('.search-results')
+    if (searchInput) {
+        searchInput.addEventListener('focus', function() {
+            isSearchInputFocused = true;
+        });
+        document.addEventListener('click', function(event) {
+            if (!searchInput.contains(event.target) && !searchResults.contains(event.target)) {
+                isSearchInputFocused = false;
+            }
+        });
+    }
+
     window.addEventListener('wheel', function(event) {
         const urlParams = new URLSearchParams(window.location.search);
-        if (!urlParams.has('m') && window.innerHeight >= 825) {
+        if (!urlParams.has('m') && window.innerHeight >= 825 && !isSearchInputFocused) {
             event.preventDefault();
             if (event.deltaY > 0) {
                 // Scrolling down
@@ -491,6 +507,27 @@ if (window.location.pathname === '/' || window.location.pathname === '/es/' || w
                     sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
                 }
             } else {
+                // Scrolling up
+                if (currentSectionIndex > 1) {
+                    sections[currentSectionIndex - 1].scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }
+            }
+        }
+    });
+
+    window.addEventListener('keydown', function(event) {
+        const urlParams = new URLSearchParams(window.location.search);
+        if (!urlParams.has('m') && window.innerHeight >= 825 && !isSearchInputFocused) {
+            event.preventDefault();
+            if (event.key === "ArrowDown") {
+                // Scrolling down
+                if (currentSectionIndex < sections.length - 1) {
+                    sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
+                }
+            } 
+            else if (event.key === "ArrowUp") {
                 // Scrolling up
                 if (currentSectionIndex > 1) {
                     sections[currentSectionIndex - 1].scrollIntoView({ behavior: 'smooth' });
